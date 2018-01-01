5f1a6fd08fda1cbe968652df7669ed6adbe54c4a
thartbm <thartbm@gmail.com>
2017-12-31 22:26:16 -0500

    create file

---
+++ nocursor.R
@@ -0,0 +1,23 @@
+# first make sure we have all necessary packages installed and loaded:
+installRequire.Packages <- function(packages) {
+  
+  installed.list <- rownames(installed.packages())
+  
+  for (pkg in packages) {
+    
+    if (!pkg %in% installed.list) {
+      install.packages(pkg,dep=TRUE)
+    }
+    
+    require(pkg, character.only=TRUE)
+    
+  }
+  
+}
+
+# not yet sure if we'll use Chi-square, or Satterthwaite estimates of F/p-values:
+# installRequire.Packages(c('nlme', 'car', 'lme4', 'lmerTest'))
+
+# localization data can be downloaded from OSF:
+groupURLs <- c('exposure'='https://osf.io/47fwu/download', 'classic'='https://osf.io/89t7j/download')
+
