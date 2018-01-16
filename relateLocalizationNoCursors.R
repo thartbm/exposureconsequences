67d80cdf3d7fd360476fe256f2ce793fbb6132e9
thartbm <thartbm@gmail.com>
2018-01-16 15:11:00 -0500

    add file for analyses that incorporate both localization and no-cursor responses

---
+++ relateLocalizationNoCursors.R
@@ -0,0 +1,13 @@
+
+source('shared.R')
+
+correlateNoCursorsLocalization <- function() {
+  
+  # first we do all responses, then split by target
+  
+  # get all data, for classic and exposure, we need localization and reach aftereffects
+  
+  
+  
+  
+}
